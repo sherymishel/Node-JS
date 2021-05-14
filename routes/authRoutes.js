877ae7eb9a8392e50bba1599e05ebfa52036a7cd
Mishel Kasianova <mishel.kasyanova.7@ukr.net>
2021-05-14 13:39:36 +0300

    Add routes update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,12 +7,18 @@ const router = Router();
 router.post('/login', (req, res, next) => {
     try {
         // TODO: Implement login action (get the user if it exist with entered credentials)
-        res.data = data;
-    } catch (err) {
-        res.err = err;
-    } finally {
-        next();
-    }
+        const {email, password} = req.body;
+                const data = AuthService.login({email});
+                if (password === data.password) {
+                  res.status(200)
+                  res.data = data;
+                }
+            } catch (err) {
+              res.status(400)
+                res.err = err;
+            } finally {
+                next();
+            }
 }, responseMiddleware);
 
 module.exports = router;
