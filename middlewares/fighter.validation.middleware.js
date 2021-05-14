204de1d3751519b095d65fdba41174a22264b2da
Mishel Kasianova <mishel.kasyanova.7@ukr.net>
2021-05-14 13:30:02 +0300

    Add middleware update

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -2,11 +2,56 @@ const { fighter } = require('../models/fighter');
 
 const createFighterValid = (req, res, next) => {
     // TODO: Implement validatior for fighter entity during creation
+    const {body} = req;
+    let notValid = [];
+    if(body.hasOwnProperty('id')){
+      notValid.push('reqest body has "id" property');
+    }
+      Object.keys(fighter).map(e=>{
+        let text = e.replace(/([a-z\d])([A-Z])/g, '$1' + " " + '$2')
+            .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + " " + '$2')
+            .toLowerCase();
+      if((e!=='health')&&(!body.hasOwnProperty(e))&&(e!=='id')){
+      notValid.push(`${text} is empty`);
+      }
+      if(e!=='health'&&(e!=='id')&&(!fighter[e](body[e]))){
+          notValid.push(`${text} is not valid`);
+      }
+    });
+    if(notValid.length <= 0){
+      console.log(body);
+      return res.status(200).json({fighter:{name, health, power, defense} = body});
+    }else if(notValid.length > 0){
+      return res.status(400).json({error:true,message:notValid[0]})
+    }
     next();
 }
 
 const updateFighterValid = (req, res, next) => {
     // TODO: Implement validatior for fighter entity during update
+    const {body} = req;
+    let notValid = [];
+    if(body.hasOwnProperty('id')){
+      notValid.push('reqest body has "id" property');
+      next();
+    }
+      Object.keys(fighter).map(e=>{
+        let text = e.replace(/([a-z\d])([A-Z])/g, '$1' + " " + '$2')
+            .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + " " + '$2')
+            .toLowerCase();
+      if((e!=='health')&&(!body.hasOwnProperty(e))&&(e!=='id')){
+      notValid.push(`${text} is empty`);
+      }
+      if(e!=='health'&&(e!=='id')&&(!fighter[e](body[e]))){
+          notValid.push(`${text} is not valid`);
+      }
+    });
+    if(notValid.length <= 0){
+      console.log(body);
+      return res.status(200).json({fighter:{name, health, power, defense} = body});
+    }else if(notValid.length > 0){
+      return res.status(400).json({error:true,message:notValid[0]})
+    }
     next();
 }
 
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,15 +1,58 @@
 const { user } = require('../models/user');
-const createUserValid = (req, res, next) => {
-    // TODO: Implement validatior for user entity during creation
 
-    next();
+const createUserValid = (req, res, next) => {
+  // TODO: Implement validation for user entity during creation
+const {body} = req;
+let notValid = [];
+if(body.hasOwnProperty('id')){
+  notValid.push('reqest body has "id" property');
+  next();
 }
+  Object.keys(user).map(e=>{
+    let text = e.replace(/([a-z\d])([A-Z])/g, '$1' + " " + '$2')
+        .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + " " + '$2')
+        .toLowerCase();
+  if(!body.hasOwnProperty(e)&&e!=='id'){
+  notValid.push(`${text} is empty`);
+  }
+  if((e!=='id')&&(!user[e](body[e]))){
+      notValid.push(`${text} is not valid`);
+  }
+});
+if(notValid.length <= 0){
+  return res.status(200).json({user:{firstName, lastName, phoneNumber, email, password} = body});
+}else if(notValid.length > 0){
+  return res.status(400).json({error:true,message:notValid[0]})
+}
+  next();
+};
 
 const updateUserValid = (req, res, next) => {
-    // TODO: Implement validatior for user entity during update
-
+  // TODO: Implement validation for user entity during update
+  const {body} = req;
+  let notValid = [];
+  if(body.hasOwnProperty('id')){
+    notValid.push('reqest body has "id" property');
     next();
-}
+  }
+    Object.keys(user).map(e=>{
+      let text = e.replace(/([a-z\d])([A-Z])/g, '$1' + " " + '$2')
+          .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + " " + '$2')
+          .toLowerCase();
+    if(!body.hasOwnProperty(e)&&e!=='id'){
+    notValid.push(`${text} is empty`);
+    }
+    if((e!=='id')&&(!user[e](body[e]))){
+        notValid.push(`${text} is not valid`);
+    }
+  });
+  if(notValid.length <= 0){
+    return res.status(200).json({user:{firstName, lastName, phoneNumber, email, password} = body});
+  }else if(notValid.length > 0){
+    return res.status(400).json({error:true,message:notValid[0]})
+  }
+  next();
+};
 
 exports.createUserValid = createUserValid;
 exports.updateUserValid = updateUserValid;
