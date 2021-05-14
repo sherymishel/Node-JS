04636a37073cb01776cd17f58b7bf7131f0f6c2c
Mishel Kasianova <mishel.kasyanova.7@ukr.net>
2021-05-14 14:20:09 +0300

    Return models

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 exports.user = {
     id: '',
-    firstName: (value)=>value === ''?false:true,
-    lastName: (value)=>value === ''?false:true,
-    email: (value)=>/(\W|^)[\w.+\-]*@gmail\.com(\W|$)/gi.test(value),
-    phoneNumber: (value)=>/^\+380(\d{9})$/g.test(value),
-    password: (value)=>(""+value).length >=3, // min 3 symbols
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    password: '' // min 3 symbols
 }
